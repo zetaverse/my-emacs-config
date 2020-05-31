fa85fe863aeaa0d8ddfe1b9e70aa1e9c5181e0b8
Denzil <denzil@zetaverse.in>
2020-05-31 09:43:54 +0530

    Routine update

--- init.el
+++ init.el
@@ -2,33 +2,18 @@
 (let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
                     (not (gnutls-available-p))))
        (proto (if no-ssl "http" "https")))
-  (when no-ssl
-    (warn "\
+  (when no-ssl (warn "\
 Your version of Emacs does not support SSL connections,
 which is unsafe because it allows man-in-the-middle attacks.
 There are two things you can do about this warning:
 1. Install an Emacs version that does support SSL and be safe.
 2. Remove this warning from your init file so you won't see it again."))
-  ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
   (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
+  ;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
+  ;; and `package-pinned-packages`. Most users will not need or want to do this.
   ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
-  (when (< emacs-major-version 24)
-    ;; For important compatibility libraries like cl-lib
-    (add-to-list 'package-archives (cons "gnu" (concat proto "://elpa.gnu.org/packages/")))))
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
+  )
 (package-initialize)
-
-(when (>= emacs-major-version 24)
-  (require 'package)
-  (add-to-list
-   'package-archives
-   ;; '("melpa" . "http://stable.melpa.org/packages/") ; many packages won't show if using stable
-   '("melpa" . "http://melpa.org/packages/")
-   t))
-   
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -36,29 +21,28 @@ There are two things you can do about this warning:
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(cua-mode t nil (cua-base))
- '(custom-enabled-themes (quote (doom-peacock)))
+ '(custom-enabled-themes (quote (doom-moonlight)))
  '(custom-safe-themes
    (quote
-    ("7d708f0168f54b90fc91692811263c995bebb9f68b8b7525d0e2200da9bc903c" default)))
- '(font-use-system-font t)
- '(initial-buffer-choice "~/Dropbox/org/tasks.org")
+    ("85837dbba3c4e77ffcda9d5d1289613b27484b6c4cb232fc2ffaa2e2ebd5712f" "7d708f0168f54b90fc91692811263c995bebb9f68b8b7525d0e2200da9bc903c" default)))
+ '(inhibit-startup-screen t)
+ '(initial-buffer-choice "C:\\Users\\lenovo\\Dropbox\\org")
  '(org-agenda-files
    (quote
-    ("~/Dropbox/org/tasks.org" "~/Dropbox/org/personal.org")))
+    ("C:\\Users\\Lenovo\\Dropbox\\org\\study.org" "C:\\Users\\Lenovo\\Dropbox\\org\\personal.org" "C:\\Users\\Lenovo\\Dropbox\\org\\tasks.org")))
  '(package-selected-packages
    (quote
-    (markdown-mode real-auto-save doom-modeline doom-themes org-bullets restart-emacs treemacs)))
+    (markdown-mode markdown-mode+ writeroom-mode use-package treemacs org-autolist org-bullets restart-emacs doom-modeline doom-themes)))
  '(scroll-bar-mode nil)
- '(size-indication-mode t)
  '(tool-bar-mode nil)
- '(treemacs-width 25))
+ '(tooltip-mode nil)
+ '(treemacs-width 28))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Monaco" :foundry "APPL" :slant normal :weight normal :height 110 :width normal)))))
-
+ '(default ((t (:family "Fira Code" :foundry "outline" :slant normal :weight normal :height 113 :width normal)))))
 
 ;; use C-x for cut
 ;; C-c for copy
@@ -92,10 +76,6 @@ There are two things you can do about this warning:
 ;; avoids file conflicts when multiple instances of emacs are running
 (global-auto-revert-mode t)
 
-;; org source blocks requirements
-;; (require 'org-tempo)
-;; using tab key to indent within src block
-;; (setq org-src-tab-acts-natively t)
 
 ;; Auto-save setting for other files
 (require 'real-auto-save)
@@ -108,7 +88,6 @@ There are two things you can do about this warning:
   (auto-save-mode t)
   (setq auto-save-interval 10))
 
-
 (use-package doom-themes
   :config
   ;; Global settings (defaults)
@@ -118,8 +97,6 @@ There are two things you can do about this warning:
   ;; Enable flashing mode-line on errors
   (doom-themes-visual-bell-config)
   
-  ;; Enable custom neotree theme (all-the-icons must be installed!)
-  (doom-themes-neotree-config)
   ;; or for treemacs users
   (setq doom-themes-treemacs-theme "doom-colors") ; use the colorful treemacs theme
   (doom-themes-treemacs-config)
